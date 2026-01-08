5e6de878f51befffa52fe237fcf023cf8a1fdaa1
Kamil Tonkowicz <kamil.tonkowicz@gmail.com>
2026-01-08 16:43:52 +0100

    style: make Car Scout placeholder font thicker (extrabold) on staging

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,7 +76,7 @@ export function Footer() {
               loading="lazy"
             />
           ) : (
-            <span className="text-xl font-bold text-white">
+            <span className="text-xl font-extrabold text-white">
               Car Scout
             </span>
           )}
